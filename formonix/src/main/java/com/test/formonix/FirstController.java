39b725ecf98fa48e20695dbd4722d536fc195dd2
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-02-15 19:27:42 -0500

    post

--- formonix/src/main/java/com/test/formonix/FirstController.java
+++ formonix/src/main/java/com/test/formonix/FirstController.java
@@ -1,9 +1,11 @@
 package com.test.formonix;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class FirstController {
@@ -16,6 +18,18 @@ public class FirstController {
     @GetMapping("/hello-2")
     public String sayHello2() {
         return "Hello 2 from my first controller";
+    }
+
+    @PostMapping("/post")
+    public String post(
+            @RequestBody String message) {
+        return "Request accepted and message is: " + message;
+    }
+
+    @PostMapping("/post-order")
+    public String post(
+            @RequestBody Order order) {
+        return "Request accepted and message is: " + order.toString();
     }
 
 }

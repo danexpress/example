62025356b384a3457fa3f1c575059f1b647f37d1
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-02-17 18:09:28 -0500

    test

--- formonix/src/test/java/com/test/formonix/student/StudentMapperTest.java
+++ formonix/src/test/java/com/test/formonix/student/StudentMapperTest.java
@@ -1,24 +1,44 @@
 package com.test.formonix.student;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StudentMapperTest {
 
+    private StudentMapper mapper;
+
     @BeforeEach
     void setUp() {
-        System.out.println("Inside the before each method");
+        mapper = new StudentMapper();
     }
 
     @Test
-    public void testMethod1() {
-        System.out.println("My first test method");
+    public void shouldMapStudentDtoStudent() {
+        StudentDto dto = new StudentDto("John", "Doe", "johndoe@mail.com", 1);
+
+        Student student = mapper.toStudent(dto);
+        assertEquals(dto.firstname(), student.getFirstname());
+        assertEquals(dto.lastname(), student.getLastname());
+        assertEquals(dto.email(), student.getEmail());
+        assertNotNull(student.getSchool());
+        assertEquals(dto.schoolId(), student.getSchool().getId());
 
     }
 
     @Test
-    public void testMethod2() {
-        System.out.println("My second test method");
+    public void shouldMapStudentDtoStudentResponseDto() {
+        Student student = new Student("John", "Doe", "johndoe@mail.com", 20);
+
+        StudentResponseDto response = mapper.toStudentResponseDto(student);
+        assertEquals(response.firstname(), student.getFirstname());
+        assertEquals(response.lastname(), student.getLastname());
+        assertEquals(response.email(), student.getEmail());
+
     }
 
 }
